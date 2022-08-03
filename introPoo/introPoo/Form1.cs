74a3e316f2d9e97e3d06cdcaae92af8296e11985
Henrique Vieira <72998582+henrique24k@users.noreply.github.com>
2022-08-03 19:58:05 -0300

    Add files via upload

---
+++ introPoo/introPoo/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace introPoo
+{
+    public partial class Form1 : Form
+    {
+        Pessoa p1 = new Pessoa();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExibir_Click(object sender, EventArgs e)
+        {
+            p1.nome = txtNome.Text;
+            p1.sobrenome = txtSobrenome.Text;
+            p1.idade = int.Parse(txtIdade.Text);
+
+            p1.Falar();
+            txtExibir.Text = p1.nomeCompleto(p1.nome, p1.sobrenome);
+
+            lblSituacao.Text = p1.situacao(p1.idade);
+
+
+
+        }
+    }
+}
---
+++ introPoo/introPoo/Pessoa.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace introPoo
+{
+    class Pessoa
+    {
+        public string nome;
+        public string sobrenome;
+        public int idade;
+
+        public void Falar()
+        {
+            System.Windows.Forms.MessageBox.Show("Olá meu nome  é " + nome + " e tenho " + idade + " anos.");
+        
+        }
+
+        public string nomeCompleto(string nome, string sobrenome)
+        {
+            return nome + " " + sobrenome;
+        }
+
+        public string situacao(int idade)
+        {
+            if(idade >= 18)
+            {
+                return "maior de idade";
+            }
+            else
+            {
+                return "Menor de idade";
+            }
+        }
+    }
+}
