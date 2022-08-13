1515e3276e98bdc3ff889664b071f58495eea851
Henrique Vieira <72998582+henrique24k@users.noreply.github.com>
2022-08-13 07:25:54 -0300

    Add files via upload

---
+++ autoCompleteCep/autoCompleteCep/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace autoCompleteCep
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtCep_Leave(object sender, EventArgs e)
+        {
+            CEP_CorreioService(txtCep.Text);
+        }
+
+        private void CEP_CorreioService(String CEP)
+        {
+            refCorreios.AtendeClienteClient correios = new refCorreios.AtendeClienteClient();
+            refCorreios.enderecoERP consulta = correios.consultaCEP(CEP);
+
+            if (consulta != null)
+            {
+                txtEnd.Text = consulta.end;
+                txtBairro.Text = consulta.bairro;
+                txtCid.Text = consulta.cidade;
+                txtUf.Text = consulta.uf;
+
+                txtNum.Focus();
+
+            }
+            else
+            {
+                MessageBox.Show("Cep não encontrado!");
+            }
+        }
+    }
+}
