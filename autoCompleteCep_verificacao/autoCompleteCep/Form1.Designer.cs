d1917b3fb3436b5562d8f453cae0cf6bfacee882
Henrique Vieira <72998582+henrique24k@users.noreply.github.com>
2022-08-20 17:56:47 -0300

    Add files via upload

---
+++ autoCompleteCep_verificacao/autoCompleteCep/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿
+namespace autoCompleteCep
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCep = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtEnd = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNum = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtComp = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtBairro = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtCid = new System.Windows.Forms.TextBox();
+            this.txtUf = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(239, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Digite o CEP";
+            // 
+            // txtCep
+            // 
+            this.txtCep.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCep.ForeColor = System.Drawing.Color.Black;
+            this.txtCep.Location = new System.Drawing.Point(243, 109);
+            this.txtCep.Name = "txtCep";
+            this.txtCep.Size = new System.Drawing.Size(208, 28);
+            this.txtCep.TabIndex = 1;
+            this.txtCep.TextChanged += new System.EventHandler(this.txtCep_TextChanged);
+            this.txtCep.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtCep_KeyDown);
+            this.txtCep.Leave += new System.EventHandler(this.txtCep_Leave);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(239, 202);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 22);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Endereço";
+            // 
+            // txtEnd
+            // 
+            this.txtEnd.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEnd.ForeColor = System.Drawing.Color.Black;
+            this.txtEnd.Location = new System.Drawing.Point(243, 241);
+            this.txtEnd.Name = "txtEnd";
+            this.txtEnd.Size = new System.Drawing.Size(386, 28);
+            this.txtEnd.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(679, 202);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 22);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "n°";
+            // 
+            // txtNum
+            // 
+            this.txtNum.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNum.ForeColor = System.Drawing.Color.Black;
+            this.txtNum.Location = new System.Drawing.Point(651, 241);
+            this.txtNum.Name = "txtNum";
+            this.txtNum.Size = new System.Drawing.Size(82, 28);
+            this.txtNum.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(775, 202);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(131, 22);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Complemento";
+            // 
+            // txtComp
+            // 
+            this.txtComp.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtComp.ForeColor = System.Drawing.Color.Black;
+            this.txtComp.Location = new System.Drawing.Point(779, 241);
+            this.txtComp.Name = "txtComp";
+            this.txtComp.Size = new System.Drawing.Size(270, 28);
+            this.txtComp.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(248, 356);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(64, 22);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Bairro";
+            // 
+            // txtBairro
+            // 
+            this.txtBairro.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBairro.ForeColor = System.Drawing.Color.Black;
+            this.txtBairro.Location = new System.Drawing.Point(252, 402);
+            this.txtBairro.Name = "txtBairro";
+            this.txtBairro.Size = new System.Drawing.Size(270, 28);
+            this.txtBairro.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(248, 465);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(73, 22);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Cidade";
+            // 
+            // txtCid
+            // 
+            this.txtCid.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCid.ForeColor = System.Drawing.Color.Black;
+            this.txtCid.Location = new System.Drawing.Point(243, 504);
+            this.txtCid.Name = "txtCid";
+            this.txtCid.Size = new System.Drawing.Size(270, 28);
+            this.txtCid.TabIndex = 11;
+            // 
+            // txtUf
+            // 
+            this.txtUf.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUf.ForeColor = System.Drawing.Color.Black;
+            this.txtUf.Location = new System.Drawing.Point(243, 616);
+            this.txtUf.Name = "txtUf";
+            this.txtUf.Size = new System.Drawing.Size(82, 28);
+            this.txtUf.TabIndex = 12;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(248, 570);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(72, 22);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Estado";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1308, 792);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtUf);
+            this.Controls.Add(this.txtCid);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtBairro);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtComp);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtNum);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtEnd);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtCep);
+            this.Controls.Add(this.label1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Buscar CEP";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCep;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtEnd;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNum;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtComp;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtBairro;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtCid;
+        private System.Windows.Forms.TextBox txtUf;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ autoCompleteCep_verificacao/autoCompleteCep/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace autoCompleteCep
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtCep_Leave(object sender, EventArgs e)
+        {
+            CEP_CorreioService(txtCep.Text);
+        }
+
+        private void CEP_CorreioService(String CEP)
+        {
+
+            try
+            {
+                refCorreios.AtendeClienteClient correios = new refCorreios.AtendeClienteClient();
+                refCorreios.enderecoERP consulta = correios.consultaCEP(CEP);
+                if (consulta != null)
+                {
+                    txtEnd.Text = consulta.end;
+                    txtBairro.Text = consulta.bairro;
+                    txtCid.Text = consulta.cidade;
+                    txtUf.Text = consulta.uf;
+                    txtNum.Focus();
+                }
+                else
+                {
+                    MessageBox.Show("Cep não encontrado!");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Cep inválido!!");
+            }
+
+        }
+
+        private void txtCep_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtCep_KeyDown(object sender, KeyEventArgs e)
+        {
+            {
+            if (e.KeyCode == Keys.Enter)
+            {
+                this.SelectNextControl(this.ActiveControl, !e.Shift, true, true, true);
+            }
+        }
+
+        }
+    }
+}
